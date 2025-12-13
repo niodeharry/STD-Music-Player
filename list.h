b04b0d66628db32d2eacf7cbdde6c4895d541f5c
niodeharry <niodeharry@gmail.com>
2025-12-14 01:05:05 +0700

    Update list.h: Penambahaan primitif

--- list.h
+++ list.h
@@ -12,4 +12,42 @@
 #define last(L) ((L).last)
 
 using namespace std;
+
+struct infotype{
+    int id;
+    string judul;
+    string artis;
+    string genre;
+    int tahun;
+};
+
+typedef struct ElmList *address;
+typedef struct ElmEdge *addressEdge;
+
+struct ElmEdge{
+    address nodeTuju;
+    addressEdge next;
+};
+
+struct ElmList{
+    infotype info;
+    address next;
+    address prev;
+
+    addressEdge firstedge;
+};
+
+struct List{
+    address first;
+    address last;
+};
+
+void createList(List &L);
+address alokasi(infotype X);
+void insertLast(List &L, address P);
+void printInfo(List L);
+
+void addEdge(address PAsal, address PTuju);
+void connectGraph(List L, address PBaru);
+
 #endif
