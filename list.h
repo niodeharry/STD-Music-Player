3ff3a3b48040c376426a1210905ccf1bdc52756d
niodeharry <niodeharry@gmail.com>
2025-12-14 01:00:01 +0700

    Inisialiasi awal

---
+++ list.h
@@ -0,0 +1,15 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <iostream>
+#include <string>
+
+#define Nil NULL
+#define info(P) (P)->info
+#define next(P) (P)->next
+#define prev(P) (P)->prev
+#define first(L) ((L).first)
+#define last(L) ((L).last)
+
+using namespace std;
+#endif
